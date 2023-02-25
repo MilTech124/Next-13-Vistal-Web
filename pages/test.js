8736eac5ee20eb8eed03143f72ed82f2c53a5e9d
MilTech124 <Jaroslawmatusiak124@gmail.com>
2023-02-25 17:32:17 +0100

    adding a config 3d

--- pages/test.js
+++ pages/test.js
@@ -1,22 +1,64 @@
 import React, { useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import Floor from "../components/Configurator/Floor";
-import Box from "../components/Configurator/Box";
-import LightBulb from "../components/Configurator/LightBulb";
-import { OrbitControls } from "@react-three/drei";
-
+import { OrbitControls, Sky, Environment,ContactShadows,BakeShadows,PivotControls } from "@react-three/drei";
+import { Blaszak } from "../components/Configurator/Blaszak";
 
 function Test() {
-    const [box,setBox]=useState()
+  const [box, setBox] = useState({
+    height: 2.5,
+    width: 2.5,
+    depth: 2.5,
+  });
+  const env =
+    "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/scythian_tombs_2k.hdr";
+  const onChange = (e) => {
+    setBox((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+  const change = () => {
+    console.log(box);
+  };
   return (
     <div id="canvas-container" className="w-[100vw] h-[100vh]">
-    <input id="in"></input>
-      <Canvas shadows={true} camera={{ position: [-6, 10, 10] }}>
-        <ambientLight color={"white"} intensity={0.3} />
-        <LightBulb position={[0, 3, 0]} />
-         <Box rotateX={3} rotateY={0.2} />
-         <OrbitControls></OrbitControls>
-        <Floor position={[0, -1, 0]} />
+      <div>
+        <h3 className="text-xl">Sterowanie boxem</h3>
+        height
+        <input
+          className="bg-slate-300 rounded-lg "
+          id="height"
+          type="text"
+          value={box.height}
+          onChange={onChange}
+        />
+        width
+        <input
+          className="bg-slate-300 rounded-lg"
+          id="width"
+          type="text"
+          value={box.width}
+          onChange={onChange}
+        />depth
+        <input
+          className="bg-slate-300 rounded-lg"
+          id="depth"
+          type="text"
+          value={box.depth}
+          onChange={onChange}
+        />
+      </div>
+
+      <button onClick={change}>Zmien</button>
+      <Canvas shadows camera={{ position: [-6, 6, 12],fov:30 }}>
+        <Environment files={env} ground={{ height: 5, radius: 40, scale: 17 }} />     
+        <ambientLight color={"white"} intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -5, -10]} />       
+        <Blaszak box={box}/>
+        <OrbitControls minPolarAngle={0} maxPolarAngle={Math.PI / 2.1} />      
+        <ContactShadows position={[0, -0, 0]} opacity={1.75} scale={20} blur={2.5} far={8} />
+        <BakeShadows />
       </Canvas>
     </div>
   );
